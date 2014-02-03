2db252385134fd39b687ac74125a1fab72bac1fd
Hans Rogério Zimermann <zrhans@gmail.com>
2014-02-03 04:06:27 -0200

    Update thais_mruv.f90

--- thais_mruv.f90
+++ thais_mruv.f90
@@ -1,13 +1,13 @@
 PROGRAM mruv
       IMPLICIT NONE                ! isto nos obriga a declarar todas as
-                             ! variaveis que usaremos (recomendavel)
+                             	   ! variaveis que usaremos (recomendavel)
       REAL :: x0, x, v0, a, t, dt  ! variaveis reais (ponto flutuante)
       INTEGER :: I, N
 
       PRINT*, 'entre x0, v0, a, t, N'
       READ*, x0, v0, a, t, N
       dt = t/N       ! passo de tempo: definido pelo tempo maximo e o
-               ! numero de pontos da trajetoria
+                     ! numero de pontos da trajetoria
       t = 0
 
       DO i = 1, N
@@ -16,7 +16,8 @@ PROGRAM mruv
       t = t + dt
       END DO
       STOP
-	  ! essa equacao se declarava variavel (V) indefinida  este era o erro deste programa
-      !para corrigir o programa substitui na equacao a variavel indefinida pela variavael declarada (v0)
+	  ! essa equacao se declarava variavel (V) indefinida  
+	  ! este era o erro deste programa para corrigir o programa 
+	  ! substitui na equacao a variavel indefinida pela variavael declarada (v0)
 END PROGRAM mruv
       
